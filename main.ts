82b63bfc61bd1c676ba55543caa684bd91068a1a
dcerver <121509667+dcerver@users.noreply.github.com>
2023-08-15 11:32:57 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,18 +7,23 @@ game.setScore(0)
 game.startCountdown(60000)
 basic.forever(function () {
     if (input.acceleration(Dimension.X) > 100) {
+        music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         Picker.change(LedSpriteProperty.X, 1)
     }
     if (input.acceleration(Dimension.X) < -100) {
+        music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         Picker.change(LedSpriteProperty.X, -1)
     }
     if (input.acceleration(Dimension.Y) > 100) {
+        music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         Picker.change(LedSpriteProperty.Y, 1)
     }
     if (input.acceleration(Dimension.Y) < -100) {
+        music.play(music.createSoundExpression(WaveShape.Square, 400, 600, 255, 0, 100, SoundExpressionEffect.Warble, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         Picker.change(LedSpriteProperty.Y, -1)
     }
     if (Picker.isTouching(litter)) {
+        music.play(music.createSoundExpression(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
         game.addScore(1)
         litter.delete()
         litter = game.createSprite(randint(0, 4), randint(0, 4))
